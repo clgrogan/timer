31288f06e7bf4fec79e988100cfd10a6cf222061
Curt Grogan <curtgrogan@outlook.com>
2019-11-06 10:11:59 -0500

    initial thing

--- public/main.js
+++ public/main.js
@@ -1,7 +1,32 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let timer = 0
+let interval
+
+const startTimer = () => {
+  // If interval is false (falsy )
+  if (!interval) {
+    // setInterval is a JS function
+    interval = setInterval(() => {
+      // syntaxtual
+      // sugar timer = timer + 1
+      timer++
+      console.log('heart beat', timer)
+      let seconds = timer
+      if (timer < 10) {
+        seconds = '0' + timer
+      }
+      document.querySelector('.seconds').textContent = seconds
+    }, 1000)
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const stopTimer = () => {
+  // do something
+  // check to see if running
+  // clearInterval is a sibling to setInterval
+  clearInterval(interval)
+  console.log(interval)
+  interval = null
+}
+
+document.querySelector('.start-timer').addEventListener('click', startTimer)
+document.querySelector('.stop-timer').addEventListener('click', stopTimer)
